dc7965b3731f4708d831a3e23add413c3d9441ae
rafaelsantosx <rafaelsantosdeoliveirax@gmail.com>
2025-01-30 09:00:52 -0300

    Curso Rest - Aula 1 a 7

--- src/main/java/br/ce/rafael/rest/OlaMundo.java
+++ src/main/java/br/ce/rafael/rest/OlaMundo.java
@@ -3,12 +3,16 @@ package br.ce.rafael.rest;
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 
 public class OlaMundo {
 
 	public static void main(String[] args) {
-		Response request = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
-		System.out.println(request.getBody().asString());
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");
+		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
+		System.out.println(response.statusCode() == 200);
+		
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(201);
 	}
-	
 }
---
+++ src/main/java/br/ce/rafael/rest/OlaMundoTest.java
@@ -0,0 +1,36 @@
+package br.ce.rafael.rest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+
+public class OlaMundoTest {
+
+	@Test
+	public void testeOlaMundo() {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");	
+		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		Assert.assertTrue(response.statusCode() == 200);
+		Assert.assertTrue("O status code deveria ser 200", response.statusCode() == 200);
+		ValidatableResponse validacao = response.then();
+		validacao.statusCode(200);
+		
+	}
+	@Test
+	public void testeOlaMundo2() {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");	
+		Assert.assertTrue(response.statusCode() == 200);
+		Assert.assertEquals("200", response.getStatusCode());
+		}
+	
+	@Test
+	public void testeOlaMundo3() {
+		Response response = RestAssured.request(Method.GET, "http://restapi.wcaquino.me/ola");	
+		Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+		
+		}
+}
